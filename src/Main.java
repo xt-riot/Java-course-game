21dcfaed9c7ce10ce264f97f3a2b9b64c4ebb2ca
Konstantinos Karachristos <karachris@csd.auth.gr>
2020-11-30 20:38:18 +0200

    sad

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, let's play Buzz!");
+        Random rant = new Random();
+        int rounds = rant.nextInt(5) + 1;
+        int questions = rant.nextInt(5) + 1;
+        System.out.println("You will play: " + rounds + " rounds.");
+        System.out.println("Each round will consist of " + questions + " questions.");
+        GameEnvironment game = new GameEnvironment(rounds, questions, 1);
+        for(int i = 0; i<rounds;i++) {
+            game.nextRound();
+        }
+        game.printTotalScore();
     }
 }
